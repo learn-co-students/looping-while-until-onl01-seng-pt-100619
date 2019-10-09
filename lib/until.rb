e0bb29aa5f585671fcfd0ac3c3297840ec581c33
allison cortez <allizetorc@gmail.com>
2019-10-09 02:01:36 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,24 @@
 def using_until
+  counter = 0
   levitation_force = 6
-  #your code here
-    
+  until levitation_force == 10
+  levitation_force += 1
+  puts "Wingardium Leviosa"
+  counter += 1
 end
+end
+
+#   levitation_force = 6
+#   while levitation_force < 10
+#   levitation_force += 1
+#   puts "Wingardium Leviosa"
+# 	end
+# end
+
+# counter = 0
+# until counter == 20
+# puts "The current number is less than 20."
+# counter += 1
+# end
+
 
